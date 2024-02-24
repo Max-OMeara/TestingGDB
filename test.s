0b77eafd861bfe5bd56d50d80707798266eb9bba
Max O'Meara <139935863+Max-OMeara@users.noreply.github.com>
2024-02-23 20:45:57 -0500

    Updated test.s

--- test.s
+++ test.s
@@ -1,17 +1,17 @@
-.intel syntax noprefix
 .section .data
 value1: .quad 0x2
 value2: .quad 0x9
 
-    .section .text
-    .global _start
+.section .text
+.global _start
 _start:
-    sub rax ,  QWORD PTR [value1]
-    cmp rax, 0
-    jg B
-A: 
-    add rax , QWORD PTR [value1]
+    mov rax, QWORD PTR [value1]    # Load value1 into rax
+    sub rax, QWORD PTR [value2]    # Subtract value2 from rax
+    cmp rax, 0                      # Compare rax with 0
+    jg B                            # Jump to B if greater than 0
+A:
+    add rax, QWORD PTR [value1]    # Add value1 to rax
 B:
-    add rax , QWORD PTR [value2]
+    add rax, QWORD PTR [value2]    # Add value2 to rax
 C:
-    int3
+    int3                            # Breakpoint
