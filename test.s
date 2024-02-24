07c0b5015e4a1fc1c74a45b09fdc97b8c041d160
Max O'Meara <139935863+Max-OMeara@users.noreply.github.com>
2024-02-23 22:15:35 -0500

    Update 2

--- test.s
+++ test.s
@@ -3,15 +3,14 @@ value1: .quad 0x2
 value2: .quad 0x9
 
 .section .text
-.global _start
+.globl _start
 _start:
-    mov rax, QWORD PTR [value1]    # Load value1 into rax
-    sub rax, QWORD PTR [value2]    # Subtract value2 from rax
-    cmp rax, 0                      # Compare rax with 0
-    jg B                            # Jump to B if greater than 0
+    subq $value1, %rax
+    cmpq $0, %rax
+    jg B
 A:
-    add rax, QWORD PTR [value1]    # Add value1 to rax
+    addq $value1, %rax
 B:
-    add rax, QWORD PTR [value2]    # Add value2 to rax
+    addq $value2, %rax
 C:
-    int3                            # Breakpoint
+    int3
